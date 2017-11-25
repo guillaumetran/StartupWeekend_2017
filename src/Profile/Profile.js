f7a7c331e1099319bb75b466db0500841a97ad98
ayzro <auguste.ziegler@epitech.eu>
2017-11-25 23:06:56 +0100

    I just posted a new pp :3

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,28 +2,41 @@
  * Created by guillaumetran on 25/11/2017.
  */
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {StyleSheet, Text, View, Image} from "react-native";
 
 export default class Profile extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Profile</Text>
-        <View>
-          <Image
-              style={{ height: "58%", width: "66%" }}
-              source={require("../assets/images/kfc.jpg")}
-              resizeMode="cover"
-          />
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Profile</Text>
+                <View style={styles_.container}>
+                    <Image
+                        style={{height: 150, width: 120}}
+                        source={require("../assets/images/pp.jpg")}
+                        resizeMode="cover"
+                        borderWidth={10}
+                        bordercolor={"red"}
+                        marginborder={15}
+                    />
+                    <Text style={{fontWeight: 'bold', fontSize: 23}}>
+                        Nom   Prenom
+                    </Text>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F8F8F8"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#F8F8F8"
+    }
+});
+const styles_ = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "#F8F8F8"
+    }
 });
