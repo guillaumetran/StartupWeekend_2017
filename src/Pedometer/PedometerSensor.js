ff11687cdf839cfe14f95b527fde2c0cc9fd1bbf
ayzro <auguste.ziegler@epitech.eu>
2017-11-25 15:25:23 +0100

    c moa ki lé fai

--- src/Pedometer/PedometerSensor.js
+++ src/Pedometer/PedometerSensor.js
@@ -1,7 +1,7 @@
 import Expo from "expo";
 import React from "react";
-import { Pedometer } from "expo";
-import { StyleSheet, Text, View } from "react-native";
+import {Pedometer} from "expo";
+import {StyleSheet, Text, View} from "react-native";
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 export default class PedometerSensor extends React.Component {
@@ -10,20 +10,12 @@ export default class PedometerSensor extends React.Component {
         pastStepCount: 0,
         currentStepCount: 0
     };
-
-    componentDidMount() {
-        this._subscribe();
-    }
-
-    componentWillUnmount() {
-        this._unsubscribe();
-    }
-
     _subscribe = () => {
         this._subscription = Pedometer.watchStepCount(result => {
             this.setState({
                 currentStepCount: result.steps
             });
+            this.reward();
         });
 
         Pedometer.isAvailableAsync().then(
@@ -44,7 +36,7 @@ export default class PedometerSensor extends React.Component {
         start.setDate(end.getDate() - 1);
         Pedometer.getStepCountAsync(start, end).then(
             result => {
-                this.setState({ pastStepCount: result.steps });
+                this.setState({pastStepCount: result.steps});
             },
             error => {
                 this.setState({
@@ -53,26 +45,45 @@ export default class PedometerSensor extends React.Component {
             }
         );
     };
-
     _unsubscribe = () => {
         this._subscription && this._subscription.remove();
         this._subscription = null;
     };
 
+    componentDidMount() {
+        this._subscribe();
+    }
+
+    componentWillUnmount() {
+        this._unsubscribe();
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>
-                    Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}
-                </Text>
-                <Text>
-                    Steps taken in the last 24 hours: {this.state.pastStepCount}
-                </Text>
-                <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+                <Text>Nombre total de pas:{this.state.pastStepCount}</Text>
+                <Text>Nombres de pas sur la journée: {this.state.currentStepCount}</Text>
+                <Text style={gras.container}>Step Up</Text>
+                <ProgressBar percent={100}/>
             </View>
         );
     }
+
+    reward() {
+        if (this.state.currentStepCount == 10)
+            ToastAndroid.show('Congratulation', ToastAndroid.SHORT);
+    }
 }
+
+const gras = StyleSheet.create({
+    container: {
+        fontWeight: "bold",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 20,
+        marginTop: 15
+    }
+});
 
 const styles = StyleSheet.create({
     container: {
