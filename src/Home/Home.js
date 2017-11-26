9b6c5cc83b72cf1175471cc8edbbfefd3eef22f4
Guillaume Tran <guillaume.tran@epitech.eu>
2017-11-26 12:38:11 +0100

    Add notification

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,163 +3,198 @@
  */
 import React from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image,
-    Platform,
-    Dimensions,
-    Button
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Platform,
+  Dimensions,
+  Button
 } from "react-native";
 import CardModal from "../Shared/CardModal";
 import ModalLine from "../Shared/ModalLine";
 import ReductionImage from "../Shared/ReductionImage";
 import { MapView, Constants, Location, Permissions } from "expo";
 
-
-const {height, width} = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 
 export default class Home extends React.Component {
-    state = {
-        reductionModal: false,
-        location: null
-    };
+  state = {
+    reductionModal: false,
+    location: null
+  };
 
-    _getLocationAsync = async () => {
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== "granted") {
-            this.setState({
-                error: true
-            });
-        }
-        let location = await Location.getCurrentPositionAsync({});
-        this.setState({ location });
-    };
-
-    componentWillMount() {
-        if (Platform.OS === "android" && !Constants.isDevice) {
-            this.setState({
-                error: true
-            });
-        } else {
-            this._getLocationAsync();
-        }
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        error: true
+      });
     }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={{flex: 1, justifyContent: "center"}}>
-                    <View
-                        style={{
-                            flex: 0.95,
-                            justifyContent: "center",
-                            flexDirection: "row"
-                        }}
-                    >
-                        <TouchableOpacity
-                            style={{flex: 0.95}}
-                            activeOpacity={0.8}
-                            onPress={() => this.setState({reductionModal: true})}
-                        >
-                            <ReductionImage
-                                image={require("../assets/images/kfc.jpg")}
-                                title="KFC"
-                                description="Jusqu'à -15% !"
-                            />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <CardModal
-                    swipeArea={height / 3}
-                    swipeThreshold={50}
-                    isOpen={this.state.reductionModal}
-                    onClosed={() => this.setState({reductionModal: false})}
-                    headerSize={0.25}
-                    backdropOpacity={0.9}
-                    header={
-                        <View style={{flex: 0.25, backgroundColor: "transparent"}}>
-                            <ModalLine/>
-                        </View>
-                    }
-                    backdropContent={
-                        <View style={{flex: 0.24, backgroundColor: "transparent"}}>
-                            <ReductionImage
-                                image={require("../assets/images/kfc.jpg")}
-                                date={true}
-                                title="KFC"
-                            />
-                        </View>
-                    }
-                >
-                    <View style={{flex: 0.25, alignItems: "center", justifyContent: "center"}}>
-                        <Text style={{flex: 0.5, fontSize: 25}} >
-                            Réaliser <Text style={{fontWeight: "bold"}}>2km </Text>dans la <Text style={{fontWeight: "bold"}} >matiné </Text>
-                        </Text>
-                    </View>
-                    <View style={styles.delimiter}/>
-                    <View style={{flex: 0.25, alignItems: "center", justifyContent: "center"}}>
-                        <Text style={{flex: 0.5, fontSize: 17}}> Profiter de vos réductions au </Text>
-                        <Text style={{flex: 0.5, fontSize: 17, textDecorationLine: "underline"}}>KFC rue du fort Geispolsheim </Text>
-                    </View>
-                    <View style={{flex: 0.5}}>
-                        <MapView
-                            initialRegion={{
-                                latitude: 48.5523076,
-                                longitude: 7.7085868,
-                                latitudeDelta: 0.0122,
-                                longitudeDelta: 0.0122
-                            }}
-                            provider={MapView.PROVIDER_GOOGLE}
-                            ref={map => (this.map = map)}
-                            showsUserLocation={true}
-                            style={{ flex: 1 }}
-                            customMapStyle={mapStyle}
-                        >
-                            <MapView.Marker
-                                coordinate={{latitude: 48.5523076,longitude: 7.7085868}}
-                                onPress={() => {
-                                    this.refs.scrollView._component.scrollTo({
-                                        x: index * (CARD_WIDTH + 10),
-                                        y: 0,
-                                        animated: false
-                                    });
-                                }}
-                            />
-                        </MapView>
-                    </View>
-                    <View style={{flex: 0.25, alignItems: "center", justifyContent: "center"}}>
-                        <TouchableOpacity
-                            onPress={this.text()}
-                            style={{flex: 0.5,
-                                backgroundColor: "#00e500",
-                                borderRadius: 5}}
-                        >
-                            <Text style={{alignItems: "center", justifyContent: "center", color: "white", fontSize: 25}}>
-                                Accepter le défi
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </CardModal>
+  componentWillMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        error: true
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <View
+            style={{
+              flex: 0.95,
+              justifyContent: "center",
+              flexDirection: "row"
+            }}
+          >
+            <TouchableOpacity
+              style={{ flex: 0.95 }}
+              activeOpacity={0.8}
+              onPress={() => this.setState({ reductionModal: true })}
+            >
+              <ReductionImage
+                image={require("../assets/images/kfc.jpg")}
+                title="KFC"
+                description="Jusqu'à -15% !"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <CardModal
+          swipeArea={height / 3}
+          swipeThreshold={50}
+          isOpen={this.state.reductionModal}
+          onClosed={() => this.setState({ reductionModal: false })}
+          headerSize={0.25}
+          backdropOpacity={0.9}
+          header={
+            <View style={{ flex: 0.25, backgroundColor: "transparent" }}>
+              <ModalLine />
             </View>
-        );
-    }
-
-    text() {
-
-    }
+          }
+          backdropContent={
+            <View style={{ flex: 0.24, backgroundColor: "transparent" }}>
+              <ReductionImage
+                image={require("../assets/images/kfc.jpg")}
+                date={true}
+                title="KFC"
+              />
+            </View>
+          }
+        >
+          <View
+            style={{
+              flex: 0.25,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ flex: 0.5, fontSize: 25 }}>
+              Réaliser <Text style={{ fontWeight: "bold" }}>2km </Text>dans la{" "}
+              <Text style={{ fontWeight: "bold" }}>matiné </Text>
+            </Text>
+          </View>
+          <View style={styles.delimiter} />
+          <View
+            style={{
+              flex: 0.25,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ flex: 0.5, fontSize: 17 }}>
+              {" "}
+              Profiter de vos réductions au{" "}
+            </Text>
+            <Text
+              style={{
+                flex: 0.5,
+                fontSize: 17,
+                textDecorationLine: "underline"
+              }}
+            >
+              KFC rue du fort Geispolsheim{" "}
+            </Text>
+          </View>
+          <View style={{ flex: 0.5 }}>
+            <MapView
+              initialRegion={{
+                latitude: 48.5523076,
+                longitude: 7.7085868,
+                latitudeDelta: 0.0122,
+                longitudeDelta: 0.0122
+              }}
+              provider={MapView.PROVIDER_GOOGLE}
+              ref={map => (this.map = map)}
+              showsUserLocation={true}
+              style={{ flex: 1 }}
+              customMapStyle={mapStyle}
+            >
+              <MapView.Marker
+                coordinate={{ latitude: 48.5523076, longitude: 7.7085868 }}
+                onPress={() => {
+                  this.refs.scrollView._component.scrollTo({
+                    x: index * (CARD_WIDTH + 10),
+                    y: 0,
+                    animated: false
+                  });
+                }}
+              />
+            </MapView>
+          </View>
+          <View
+            style={{
+              flex: 0.25,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                if (this.props.updateNotif) this.props.updateNotif();
+              }}
+              style={{
+                flex: 0.5,
+                backgroundColor: "#00e500",
+                borderRadius: 5
+              }}
+            >
+              <Text
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "white",
+                  fontSize: 25
+                }}
+              >
+                Accepter le défi
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </CardModal>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#F8F8F8"
-    },
-    delimiter: {
-        borderBottomColor: "gray",
-        borderBottomWidth: StyleSheet.borderWidth,
-        borderWidth: 0.5
-    }
-
+  container: {
+    flex: 1,
+    backgroundColor: "#F8F8F8"
+  },
+  delimiter: {
+    borderBottomColor: "gray",
+    borderBottomWidth: StyleSheet.borderWidth,
+    borderWidth: 0.5
+  }
 });
--- src/Shared/TabBar.js
+++ src/Shared/TabBar.js
@@ -8,18 +8,32 @@ import Home from "../Home/Home";
 import AroundMe from "../AroundMe/AroundMe";
 import Challenge from "../Challenge/Challenge";
 import Profile from "../Profile/Profile";
-
-const pages = [
-  { name: "Accueil", icon: "home", component: <Home /> },
-  { name: "Proche", icon: "location-arrow", component: <AroundMe /> },
-  { name: "Défis", icon: "trophy", component: <Challenge /> },
-  { name: "Profile", icon: "user", component: <Profile /> }
-];
+import IconBadge from "react-native-icon-badge";
 
 export default class TabBar extends React.Component {
   state = {
-    selectedIndex: 0
+    selectedIndex: 0,
+    notifDefis: 0
   };
+
+  pages = [
+    {
+      name: "Accueil",
+      icon: "home",
+      component: <Home updateNotif={() => this.updateNotif()} />
+    },
+    {
+      name: "Proche",
+      icon: "location-arrow",
+      component: <AroundMe updateNotif={() => this.updateNotif()} />
+    },
+    { name: "Défis", icon: "trophy", component: <Challenge /> },
+    { name: "Profile", icon: "user", component: <Profile /> }
+  ];
+
+  updateNotif() {
+    this.setState({ notifDefis: this.state.notifDefis + 1 });
+  }
 
   selectPage(item, index) {
     this.props.selectPage(item.component);
@@ -29,7 +43,59 @@ export default class TabBar extends React.Component {
     return (
       <View style={styles.container}>
         <View style={{ flex: 1, flexDirection: "row" }}>
-          {pages.map((item, index) => {
+          {this.pages.map((item, index) => {
+            if (index === 2 && this.state.notifDefis > 0) {
+              return (
+                <IconBadge
+                  key={index}
+                  MainElement={
+                    <TouchableOpacity
+                      key={index}
+                      style={styles.button}
+                      onPress={() => this.selectPage(item, index)}
+                    >
+                      <FontAwesome
+                        color={
+                          this.state.selectedIndex === index
+                            ? "#007AFF"
+                            : "#8E8E93"
+                        }
+                        name={item.icon}
+                        size={32}
+                      />
+                      <Text
+                        style={[
+                          styles.text,
+                          {
+                            color:
+                              this.state.selectedIndex === index
+                                ? "#007AFF"
+                                : "#8E8E93"
+                          }
+                        ]}
+                      >
+                        {item.name}
+                      </Text>
+                    </TouchableOpacity>
+                  }
+                  BadgeElement={
+                    <Text style={{ color: "#FFFFFF" }}>
+                      {this.state.notifDefis}
+                    </Text>
+                  }
+                  IconBadgeStyle={{
+                    width: 25,
+                    height: 20,
+                    backgroundColor: "#D61700",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    top: 0,
+                    right: 0
+                  }}
+                  Hidden={false}
+                />
+              );
+            }
             return (
               <TouchableOpacity
                 key={index}
